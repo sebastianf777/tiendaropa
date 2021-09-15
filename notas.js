1b2eb884cfdde414b727adf4a2e76b703df02ccb
Sebastian Fontana <sebastianf777@outlook.com>
2021-09-15 08:32:24 -0300

    agrego navbar

---
+++ notas.js
@@ -0,0 +1,11 @@
+/*componente funcional siempre en mayuscula
+se puede poner style pero asi
+let style = {
+    color: "red";
+}
+
+const stylesJsx{
+    <div style={style}>
+    <p>{jsx}<p/>
+}
+*/
